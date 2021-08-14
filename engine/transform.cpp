3badeffbc485497bc50b424ac7ebb52fb36543ab
Alexander Veselov <Alexander_Veselov@yahoo.com>
2021-08-14 23:39:58 +0200

    Cleanup some stuff

--- engine/transform.cpp
+++
@@ -1,9 +0,0 @@
-#include "transform.hpp"
-#include "component_manager.hpp"
-
-REGISTER_COMPONENT_CLASS(Transform, transform);
-
-void Transform::Ping() const
-{
-    printf("Transform::Ping()\n");
-}
